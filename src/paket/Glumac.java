55065b376777f9c049a0c02a03490b22f64256df
mjelenabg <mjelenabg@gmail.com>
2015-05-11 01:13:46 +0100

    Napravljena klasa Glumac.

---
+++ src/paket/Glumac.java
@@ -0,0 +1,39 @@
+package paket;
+
+public class Glumac {
+	public int visina;
+	public int godine;
+	public int brojNagrada;
+	public String pol;
+	public int getVisina() {
+		return visina;
+	}
+	public void setVisina(int visina) {
+		this.visina = visina;
+	}
+	public int getGodine() {
+		return godine;
+	}
+	public void setGodine(int godine) {
+		this.godine = godine;
+	}
+	public int getBrojNagrada() {
+		return brojNagrada;
+	}
+	public void setBrojNagrada(int brojNagrada) {
+		this.brojNagrada = brojNagrada;
+	}
+	public String getPol() {
+		return pol;
+	}
+	public void setPol(String pol) {
+		this.pol = pol;
+	}
+	@Override
+	public String toString() {
+		return "Glumac [visina=" + visina + ", godine=" + godine
+				+ ", brojNagrada=" + brojNagrada + ", pol=" + pol + "]";
+	}
+	
+
+}
